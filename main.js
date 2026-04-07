c28fe64ba9523aae46fd1d6bb5867f1634123284
lgnidk <legionidk@internet.ru>
2026-04-07 20:35:32 +0300

    updated formatting logic

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
-document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault();
+const form = document.forms[0];
+console.log(form);
+
+form.addEventListener("input", (e) => {
+    // Форматирование ввода номера карты
+    if (e.target.id === "card-number-input") {
+        const cleanedValue = e.target.value.replace(/\D/g, "");
+        const formattedValue = cleanedValue.match(/.{1,4}/g);
+
+        if (formattedValue) {
+            e.target.value = formattedValue.slice(0, 4).join(" ");
+            return;
+        }
+
+        e.target.value = formattedValue;
+    }
+
+    // Форматирование ввода срока действия
+    if (e.target.id === "expires-date-input") {
+        const cleanedValue = e.target.value.replace(/\D/g, "");
+        const formattedValue = cleanedValue.match(/.{1,2}/g);
+
+        if (formattedValue) {
+            e.target.value = formattedValue.slice(0, 2).join("/");
+            return;
+        }
+
+        e.target.value = formattedValue;
+    }
 });
+
+form.addEventListener("submit", (e) => {});
